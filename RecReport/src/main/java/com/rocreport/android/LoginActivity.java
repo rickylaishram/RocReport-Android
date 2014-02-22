c2aa191b2ebb97ff07b258f79ee38d16172c7e65
Ricky Laishram <rickylaishram@gmail.com>
2014-02-22 00:02:14 -0500

    Login Activity

---
+++ RecReport/src/main/java/com/rocreport/android/LoginActivity.java
@@ -0,0 +1,179 @@
+package com.rocreport.android;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+
+import android.app.Activity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends Activity{
+
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private EditText mPasswordView;
+    private EditText mEmailView;
+
+    private ProgressDialog pDialog;
+    private Context CTX;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        CTX = this;
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    public void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new ProgressDialog(CTX);
+            pDialog.setMessage("Sending ...");
+            pDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            pDialog.dismiss();
+
+            if (success) {
+                finish();
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            pDialog.dismiss();
+        }
+    }
+}
+
+
+
--- RecReport/src/main/java/com/rocreport/android/ReportActivity.java
+++ RecReport/src/main/java/com/rocreport/android/ReportActivity.java
@@ -1,16 +1,20 @@
 package com.rocreport.android;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,10 +23,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.util.Base64;
+
 
 import com.rocreport.utils.utils.Utils;
+import com.rocreport.utils.utils.Constants;
 
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReportActivity extends Activity {
 
@@ -30,16 +53,23 @@ public class ReportActivity extends Activity {
     private Uri fileUri;
     private static int PIC_REQUEST;
     public static final int MEDIA_TYPE_IMAGE = 1;
-    private File image;
+    private File IMAGE;
 
     private LocationManager locationManager;
     private LocationListener locationListener;
     private double longitude, latitude;
 
+    private String IMAGE_URL;
+
+    private ProgressDialog pDialog;
+    private Context CTX;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        CTX = this;
 
         Spinner spinner = (Spinner) findViewById(R.id.category);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -48,7 +78,7 @@ public class ReportActivity extends Activity {
         spinner.setAdapter(adapter);
 
         Button btnCamera = (Button) findViewById(R.id.camera);
-        //EditText etLocation = (EditText) findViewById(R.id.address);
+        Button btnSend = (Button) findViewById(R.id.send);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,12 +92,22 @@ public class ReportActivity extends Activity {
                     dir.mkdirs();
                 }
 
-                image = new File(dir.getPath() + File.separator + PIC_REQUEST + ".jpg");
-                fileUri = Uri.fromFile(image); // create a file to save the image
+                IMAGE = new File(dir.getPath() + File.separator + PIC_REQUEST + ".jpg");
+                fileUri = Uri.fromFile(IMAGE); // create a file to save the image
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
 
                 // start the image capture Intent
                 startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap scaledPhoto = Utils.decodeSampledBitmapFromFilePath(IMAGE.toString(), 500, 500);
+
+                SendData upload = new SendData();
+                upload.execute(scaledPhoto);
             }
         });
 
@@ -138,19 +178,173 @@ public class ReportActivity extends Activity {
                 //File dir = new File(Environment.getExternalStorageDirectory(), "Spiders");
                 File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "rocreport");
                 File reimage = new File(dir.getPath() + File.separator + PIC_REQUEST + ".jpg");
-                //image = reimage;
+                IMAGE = reimage;
 
+                Bitmap scaledPhoto = Utils.decodeSampledBitmapFromFilePath(reimage.toString(), 400, 400);
 
                 ImageView photo = (ImageView) findViewById(R.id.photo);
-                //photo.setImageURI(Uri.parse(reimage.toString()));
-                photo.setImageBitmap(Utils.decodeSampledBitmapFromFilePath(reimage.toString(), 400, 400));
-                //photo.setImageUri(Uri.parse(new File("/sdcard/cats.jpg").toString()));
-                //photo.setIma
+                photo.setImageBitmap(scaledPhoto);
             }else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {
                 // Image capture failed, advise user
             }
+        }
+    }
+
+    public class SendData extends AsyncTask<Bitmap, Void, Void> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new ProgressDialog(CTX);
+            pDialog.setMessage("Sending ...");
+            pDialog.show();
+        }
+
+        @Override
+        public Void doInBackground(Bitmap... bmap) {
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bmap[0].compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            String sPhoto = Base64.encodeToString(bos.toByteArray(), Base64.DEFAULT);
+
+            HttpPost hpost = new HttpPost(Constants.IMGUR_ENDPOINT+Constants.IMGUR_UPLOAD);
+
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("image", sPhoto));
+            nameValuePairs.add(new BasicNameValuePair("type", "base64"));
+
+            try
+            {
+                hpost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            } catch (UnsupportedEncodingException e) {
+                Log.e("Upload", e.toString());
+            }
+
+            hpost.setHeader("Authorization", Constants.IMGUR_AUTH);
+
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpResponse resp = null;
+            try
+            {
+                resp = client.execute(hpost);
+            } catch (ClientProtocolException e)
+            {
+                Log.e("Upload", e.toString());
+            } catch (IOException e)
+            {
+                Log.e("Upload", e.toString());
+            }
+
+            String result = null;
+            try {
+                result = EntityUtils.toString(resp.getEntity());
+                JSONObject json = new JSONObject(result);
+                JSONObject data = json.getJSONObject("data");
+                IMAGE_URL = data.getString("link");
+                Log.v("Image", IMAGE_URL+"");
+                int i = 0;
+            } catch (Exception e) {
+                Log.e("Upload", e.toString());
+            }
+
+            //Upload Location
+            /*final String temp_lat = latitude_user+"";
+            final String temp_lng = longitude_user+"";
+
+            RequestParams params = new RequestParams();
+            params.put("location[latitude]",temp_lat);
+            params.put("location[longitude]",temp_lng);
+            params.put("location[radius]",50);*/
+
+            /*AsyncHttpClient locationclient = new AsyncHttpClient();
+            locationclient.post(API_ENDPOINT + API_NEW_LOCATION, params, new AsyncHttpResponseHandler(){
+
+                @Override
+                public void onStart() {
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody){
+                    AsyncHttpClient client1 = new AsyncHttpClient();
+                    client1.get(API_ENDPOINT + API_GET_LOCATIONS, new AsyncHttpResponseHandler() {
+
+                        @Override
+                        public void onStart() {
+                        }
+
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                            int id = 0;
+
+                            try {
+                                JSONArray json = new JSONArray(new String(responseBody));
+                                for(int i = 0; i < json.length(); i++) {
+                                    JSONObject element = json.getJSONObject(i);
+                                    String lat = element.getDouble("latitude")+"";
+                                    String lng = element.getDouble("longitude")+"";
+
+                                    if(lat.equals(temp_lat) && lng.equals(temp_lng)) {
+
+                                        locationid = element.getInt("id")+"";
+
+                                        break;
+                                    }
+                                }
+                            } catch (Exception e) {
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        }
+
+                        @Override
+                        public void onRetry() {
+                            // Request was retried
+                        }
+
+                        @Override
+                        public void onProgress(int bytesWritten, int totalSize) {
+                            // Progress notification
+                        }
+
+                        @Override
+                        public void onFinish() {
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error){
+                    //tv_status.setText("Something went wrong :(");
+                }
+
+                @Override
+                public void onRetry() {
+                    // Request was retried
+                }
+
+                @Override
+                public void onProgress(int bytesWritten, int totalSize) {
+                    // Progress notification
+                }
+
+                @Override
+                public void onFinish() {
+                }
+            });*/
+
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(Void a) {
+            pDialog.dismiss();
+            //Button btn_save = (Button) findViewById(R.id.btn_save);
+            //btn_save.setEnabled(true);
         }
     }
 
