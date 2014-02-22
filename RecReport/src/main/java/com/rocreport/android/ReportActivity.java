7a9db670d4daa0909824ef985a02d5f0b229699b
Ricky Laishram <rickylaishram@gmail.com>
2014-02-21 22:36:47 -0500

    Location and photo

--- RecReport/src/main/java/com/rocreport/android/ReportActivity.java
+++ RecReport/src/main/java/com/rocreport/android/ReportActivity.java
@@ -1,13 +1,40 @@
 package com.rocreport.android;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.rocreport.utils.utils.Utils;
+
+import java.io.File;
+
 public class ReportActivity extends Activity {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri fileUri;
+    private static int PIC_REQUEST;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private File image;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double longitude, latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +46,68 @@ public class ReportActivity extends Activity {
                 R.array.category_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button btnCamera = (Button) findViewById(R.id.camera);
+        //EditText etLocation = (EditText) findViewById(R.id.address);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PIC_REQUEST = (int) System.currentTimeMillis();
+                // create Intent to take a picture and return control to the calling application
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "rocreport");
+                if(!dir.exists()) {
+                    dir.mkdirs();
+                }
+
+                image = new File(dir.getPath() + File.separator + PIC_REQUEST + ".jpg");
+                fileUri = Uri.fromFile(image); // create a file to save the image
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+                // start the image capture Intent
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        /* Location part
+		 * Continuously update while user is in activity
+		 */
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        latitude = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
+        longitude = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+
+        EditText etLocation = (EditText) findViewById(R.id.address);
+        etLocation.setText(latitude+","+longitude);
+
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            @Override
+            public void onProviderEnabled(String provider) {}
+
+            @Override
+            public void onProviderDisabled(String provider) {
+				/* If GPS is disable launch Locations Settings */
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+                EditText etLocation = (EditText) findViewById(R.id.address);
+                etLocation.setText(latitude+","+longitude);
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
 
@@ -40,6 +129,29 @@ public class ReportActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                //File dir = new File(Environment.getExternalStorageDirectory(), "Spiders");
+                File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "rocreport");
+                File reimage = new File(dir.getPath() + File.separator + PIC_REQUEST + ".jpg");
+                //image = reimage;
+
+
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                //photo.setImageURI(Uri.parse(reimage.toString()));
+                photo.setImageBitmap(Utils.decodeSampledBitmapFromFilePath(reimage.toString(), 400, 400));
+                //photo.setImageUri(Uri.parse(new File("/sdcard/cats.jpg").toString()));
+                //photo.setIma
+            }else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
     }
 
 }
